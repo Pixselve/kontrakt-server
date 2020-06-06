e719c3e6d9c6ad06e3d6e37135b8e8404a816443
Pixselve <pixselve@gmail.com>
2020-06-06 20:13:27 +0200

    Fix Query to Mutation and add Authorization

--- src/resolvers/contract/CreateOneContract.resolver.ts
+++ src/resolvers/contract/CreateOneContract.resolver.ts
@@ -1,11 +1,12 @@
-import { Arg, Ctx, Query, Resolver } from "type-graphql";
+import { Arg, Authorized, Ctx, Mutation, Query, Resolver } from "type-graphql";
 import { Context } from "../../index";
 import { Contract } from "../../models/Contract";
 import { CreateOneContractInput } from "./input/CreateOneContract.input";
 
 @Resolver(Contract)
 export default class CreateOneContractResolver {
-  @Query((returns) => Contract)
+  @Mutation((returns) => Contract)
+  @Authorized("TEACHER")
   async createOneContract(
     @Ctx() { prisma }: Context,
     @Arg("contract")
--- src/resolvers/contract/DeleteOneContract.resolver.ts
+++ src/resolvers/contract/DeleteOneContract.resolver.ts
@@ -1,10 +1,11 @@
-import { Arg, Ctx, Int, Query, Resolver } from "type-graphql";
+import { Arg, Authorized, Ctx, Int, Mutation, Query, Resolver } from "type-graphql";
 import { Contract } from "../../models/Contract";
 import { Context } from "../../index";
 
 @Resolver(Contract)
 export default class DeleteOneContractResolver {
-  @Query((returns) => Contract)
+  @Mutation((returns) => Contract)
+  @Authorized("TEACHER")
   async deleteOneContract(
     @Ctx() { prisma }: Context,
     @Arg("id", (returns) => Int) id: number
