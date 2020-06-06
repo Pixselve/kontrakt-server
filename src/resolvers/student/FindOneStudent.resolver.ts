ab2b43a0e373396817eb01e2279fb7f1e10a97ff
Pixselve <pixselve@gmail.com>
2020-06-06 20:21:07 +0200

    Find one student resolver

---
+++ src/resolvers/student/FindOneStudent.resolver.ts
@@ -0,0 +1,14 @@
+import { Arg, Ctx, Int, Query, Resolver } from "type-graphql";
+import { Student } from "../../models/Student";
+import { Context } from "../../index";
+
+@Resolver(Student)
+export default class DeleteOneStudentResolver {
+  @Query((returns) => Student)
+  async student(
+    @Ctx() { prisma }: Context,
+    @Arg("id", (returns) => Int) id: number
+  ) {
+    return prisma.student.findOne({ where: { id } });
+  }
+}
