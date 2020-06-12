9646bb8e5e995e24dd6bdcbc7e1975d21b677620
Pixselve <pixselve@gmail.com>
2020-06-12 17:48:15 +0200

    Update exposed resolvers

--- src/utils/buildSchema.ts
+++ src/utils/buildSchema.ts
@@ -9,6 +9,24 @@ import DeleteOneTeacherResolver from "../resolvers/teacher/DeleteOneTeacher.reso
 import LoginTeacherResolver from "../resolvers/teacher/LoginTeacher.resolver";
 import FindManyTeacherResolver from "../resolvers/teacher/FindManyTeacher.resolver";
 import CreateOneStudentResolver from "../resolvers/student/CreateOneStudent.resolver";
+import UpdateOneStudentResolver from "../resolvers/student/UpdateOneStudent.resolver";
+import UpdateOneContractResolver from "../resolvers/contract/UpdateOneContract.resolver";
+import CreateOneGroupResolver from "../resolvers/groups/CreateOneGroup.resolver";
+import CreateOneSkillResolver from "../resolvers/skill/CreateOneSkill.resolver";
+import DeleteOneSkillResolver from "../resolvers/skill/DeleteOneSkill.resolver";
+import UpdateOneSkillResolver from "../resolvers/skill/UpdateOneSkill.resolver";
+import LoginStudentResolver from "../resolvers/student/LoginStudent.resolver";
+import CreateOneContractResolver from "../resolvers/contract/CreateOneContract.resolver";
+import DeleteOneContractResolver from "../resolvers/contract/DeleteOneContract.resolver";
+import DeleteOneStudentResolver from "../resolvers/student/DeleteOneStudent.resolver";
+import UpsertOneSkillToStudentResolver from "../resolvers/skillToStudent/UpsertOneSkillToStudent.resolver";
+import FindManyMarkResolver from "../resolvers/mark/FindManyMark.resolver";
+import FindManyContractResolver from "../resolvers/contract/FindManyContract.resolver";
+import FindOneContractResolver from "../resolvers/contract/FindOneContract.resolver";
+import FindOneStudentResolver from "../resolvers/student/FindOneStudent.resolver";
+import MeResolver from "../resolvers/user/Me.resolver";
+import FindManyContractNotFinishedByStudent from "../resolvers/contract/FindManyContractNotFinishedByStudent";
+import CreateManyStudentCSVResolver from "../resolvers/student/CreateManyStudentCSV.resolver";
 
 export default async (): Promise<GraphQLSchema> =>
   buildSchema({
@@ -19,7 +37,25 @@ export default async (): Promise<GraphQLSchema> =>
       FindManyTeacherResolver,
       FindManyStudent,
       FindManyGroup,
-      CreateOneStudentResolver
+      CreateOneStudentResolver,
+      UpdateOneStudentResolver,
+      UpdateOneContractResolver,
+      CreateOneGroupResolver,
+      CreateOneSkillResolver,
+      DeleteOneSkillResolver,
+      UpdateOneSkillResolver,
+      LoginStudentResolver,
+      CreateOneContractResolver,
+      DeleteOneContractResolver,
+      DeleteOneStudentResolver,
+      UpsertOneSkillToStudentResolver,
+      FindManyMarkResolver,
+      FindManyContractResolver,
+      FindOneContractResolver,
+      FindOneStudentResolver,
+      MeResolver,
+      FindManyContractNotFinishedByStudent,
+      CreateManyStudentCSVResolver
     ],
     validate: true,
     authChecker: customAuthChecker,
